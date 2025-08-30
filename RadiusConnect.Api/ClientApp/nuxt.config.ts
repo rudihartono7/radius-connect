316e6a19575822e6a78774b9baf7efbc29120f38
Rudi Hartono <38203150+rudihartono7@users.noreply.github.com>
2025-08-30 13:39:03 +0700

    Update nuxt.config.ts

--- RadiusConnect.Api/ClientApp/nuxt.config.ts
+++ RadiusConnect.Api/ClientApp/nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
   ],
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:5069/api'
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || '/api'
     }
   },
   // Disable HTTPS in development
